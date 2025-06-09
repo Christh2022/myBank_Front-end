70eedeecc588cef6c4afca8b2aa4ab808bb150f6
Roldi Christh MAMPASSI <christhmoukietou@gmail.com>
2025-06-09 04:05:55 +0200

    Fixes #2 : configuration de tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ function App() {
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
+
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -21,7 +22,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <p className='text-[#FCA311]'>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
