fbc466bb4c25bcb951b0447e4d999d68349e1c7f
Roldi Christh MAMPASSI <christhmoukietou@gmail.com>
2025-06-27 05:20:40 +0200

    Feat: add AppLayout.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,48 @@
 import { RouterProvider } from "react-router/dom";
 import router from "./Router/Routes";
+import { requireAuth } from "./utils/Auth";
+import { useEffect, useState } from "react";
+import "./App.css";
 
+function LoaderBoundary() {
+  return (
+      <div style={{ padding: '2rem', textAlign: 'center' }}>
+          <div className="spinner" />
+          <p>Vérification du token...</p>
+      </div>
+  );
+}
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+
+
+  useEffect(() => {
+    const path = window.location.pathname;
+    
+    console.log(path === '/Login');
+    if (path === '/Login' || path === '/register') {
+      setLoading(false);
+      
+    } else {
+      // Vérifier le token à chaque chargement de l'application
+      setLoading(true);
+      requireAuth()
+        .catch(() => {
+            // Rediriger ou gérer l'erreur ici si besoin
+        })
+        .finally(() => setLoading(false));
+    }
+    
+  }, []);
+
 
   return (
-    <>
+    <div>
+      {loading && <LoaderBoundary />}
       <RouterProvider router={router} />
-    </>
+    </div>
   )
 }
 
---
+++ src/Layout/AppLayout.tsx
@@ -0,0 +1,14 @@
+import { Outlet, } from "react-router";
+
+
+  
+
+function AppLayout() {
+    
+    return (
+        <main>
+            <Outlet />
+        </main>
+    );
+}
+export default AppLayout;
